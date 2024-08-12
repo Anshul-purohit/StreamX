532e64cf9429b926349fc17f372f6d7efd6988b1
Anshul-purohit <anshulpurohit11@gmail.com>
2024-08-13 04:14:17 +0530

    deleted previous files

--- client/src/socketConn/index.js
+++
@@ -1,6 +0,0 @@
-export {
-  connectWithSocketServer,
-  getChatHistory,
-  sendChatMessage,
-  closeChatSubscription,
-} from "./socketConn";
--- client/src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-
-export const useStore = create((set) => ({
-  chatHistory: {
-    channelId: null,
-    messages: [],
-  },
-  setChatHistory: (chatHistory) => set({ chatHistory }),
-}));
